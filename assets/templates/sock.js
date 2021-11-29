8f81404e4dea8f16278b5a75c2ce929be7c6b3b8
C3ald <specklesjr10@gmail.com>
2021-11-29 10:50:46 -0700

    small adjustments

--- assets/templates/sock.js
+++ assets/templates/sock.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { NativeModules } from "react-native";
 
+
+
 const url = WebSocket('ws://token-network.herokuapp.com/ws');
 
 url.onmessage = function(event) {
 	var message = null
 };
+
+
+export var url;
